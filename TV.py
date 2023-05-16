e08b8a07e53e4400296e46f043ca71e4db07eb80
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-05-16 23:46:34 +0800

    [A-6] Trial progress for class TV

--- TV.py
+++ TV.py
@@ -1,1 +1,34 @@
 #In trial progress of making a class for TV
+
+class TV:
+    #Constructing a TV object
+    def __init__(self, model, power, channel, volume):
+        self.model = model
+        self.power = power
+        self.channel = channel
+        self.volume = volume
+    
+    #Constructing a Power Button Object:
+    def PowerButton(self):
+        print("The TV is turned off")
+        self.power = input("Turn on the TV? (type 'on' to turn on tv) :")
+        if self.power == "on":
+            print("*C l i c k*")
+            return "The TV is turned on"
+
+    #Constructing a Channel object
+    def TvChannel(self):
+        self.channel = int(input("Enter channel: "))
+        for i in range(0,self.channel):
+            self.channel = self.channel
+            return self.channel
+
+    
+    #Constructing a Volume Level object
+    def TvVolume(self):
+        self.volume = int(input("Enter Volume Level: "))
+        for i in range(0,self.volume):
+            self.volume = self.volume
+            return self.volume
+        
+
--- test_driver.py
+++ test_driver.py
@@ -1,39 +1,7 @@
 #Trial progress for creating <class> for TV
 
-# class TV:
-
-import math
-
-class Circle:
-    def __init__(self, radius = 1):
-        self.radius = radius
-    
-    def getPerimeter(self):
-        return 2 * self.radius * math.pi
-    
-    def getArea(self):
-        return self.radius * self.radius * math.pi
-    
-    def setRadius(self, radius):
-        self.radius = radius
-
-def main():
-    # Create a circle with radius 1
-    circle1 = Circle()
-    print("The area of the circle of radius", circle1.radius,"is",circle1.getArea())
-
-    # Create a circle with radius 25
-    circle2 = Circle(25)
-    print("The area of the circle of radius", circle2.radius, "is",circle2.getArea())
-
-    # Create a circle with radius 125
-    circle3 = Circle(125)
-    print("The area of the circle of radius", circle3.radius,  "is",circle3.getArea())
-        
-
-    #Modify circle radius
-    circle2.radius =100  # or circle2.setRadius(100)
-    print("The area of the circle radius", circle2.radius, "is", circle2.getArea())
-      
-
-main()  # Call the main function
+from TV import TV
+television_one = TV("Apple TV", "off", 2, 50)
+print(television_one.PowerButton())
+print(television_one.TvChannel())
+print(television_one.TvVolume())
