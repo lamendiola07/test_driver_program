e008e51cd35e040c4dc0a8c01b94207f235d1160
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-05-17 00:12:43 +0800

    [A-6] Testing Trial for class TV

--- test_driver.py
+++ test_driver.py
@@ -1,6 +1,7 @@
 #Trial progress for creating <class> for TV
 
 from TV import TV
+
 television_one = TV("Apple TV", "off", 2, 50)
 print(television_one.PowerButton())
 print(television_one.TvChannel())
