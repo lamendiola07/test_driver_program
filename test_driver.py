937e02d44dbfeb89eaf2938517d1236ce829722b
Logie Mendiola <logiamendiola@iskolarngbayan.pup.edu.ph>
2023-05-17 01:38:28 +0800

    [A-6] Modified class and function

--- test_driver.py
+++ test_driver.py
@@ -5,11 +5,13 @@ from TV import TV
 def TvUnit():
     #Creating a Television Model Number #1
     television_one = TV()
-    television_one = print(television_one.TvModel(),"'s channel is", television_one.TvChannel(), "and volume level is", television_one.TvVolume())
+    print(television_one.PowerButton())
+    print(television_one.TvModel(),"'s channel is", television_one.TvChannel(), "and volume level is", television_one.TvVolume())
     
     print("\n\n")
     #Creating a Television Model Number #2
     television_two = TV()
+    print(television_two.PowerButton())
     print(television_two.TvModel(),"'s channel is", television_two.TvChannel(), "and volume level is", television_two.TvVolume())
 
    
